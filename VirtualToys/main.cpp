0e566eda87ca282d08323cd70b615587df84a340
Himanshu Yadav <himanshuf22@hotmail.com>
2022-05-08 21:06:50 +0530

    Initial Commit

---
+++ VirtualToys/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VirtualToys
+//
+//  Created by Himanshu Yadav on 08/05/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
